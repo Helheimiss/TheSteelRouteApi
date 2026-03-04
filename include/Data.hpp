783ce8eac7ff35b8f224f2b2835bb4a1a03e6d8b
Helheimiss <171362259+Helheimiss@users.noreply.github.com>
2026-03-04 21:51:44 +0300

    changes

--- include/Data.hpp
+++ include/Data.hpp
@@ -31,13 +31,31 @@ namespace DATA {
         std::string Login;
         std::string PasswordHash;
         std::string CreatedAt;
+
+        friend std::size_t hash_value(const User &obj) {
+            std::hash<std::string> hasher;
+
+            std::size_t seed = 0x2D03CCAD;
+            seed ^= (seed << 6) + (seed >> 2) + 0x3EEE27F1 + hasher(obj.Id);
+            seed ^= (seed << 6) + (seed >> 2) + 0x18B109F5 + hasher(obj.LastName);
+            seed ^= (seed << 6) + (seed >> 2) + 0x3215BAE4 + hasher(obj.FirstName);
+            seed ^= (seed << 6) + (seed >> 2) + 0x4AF2B1D3 + hasher(obj.Email);
+            seed ^= (seed << 6) + (seed >> 2) + 0x3E1DBBC1 + hasher(obj.Phone);
+            seed ^= (seed << 6) + (seed >> 2) + 0x6ED35753 + hasher(obj.Login);
+            seed ^= (seed << 6) + (seed >> 2) + 0x65533FFB + hasher(obj.PasswordHash);
+            seed ^= (seed << 6) + (seed >> 2) + 0x243A8F37 + hasher(obj.CreatedAt);
+            return seed;
+        }
     };
 
     inline const std::string SECRET_KEY = "secret";
-    inline std::unordered_set<std::string> Tokens;
-
+    inline QHash<std::size_t, std::string> Tokens;
 
     inline std::string createToken(const User& usr) {
+        if (Tokens.contains(hash_value(usr))) {
+            return Tokens[hash_value(usr)];
+        }
+
         auto token= jwt::create()
             .set_type("JWT")
             .set_subject(usr.Id)
@@ -50,7 +68,7 @@ namespace DATA {
             .set_issued_at(std::chrono::system_clock::now())
             .sign(jwt::algorithm::hs256{SECRET_KEY});
 
-        Tokens.insert(token);
+        Tokens.insert(hash_value(usr), token);
         return token;
     }
 }
