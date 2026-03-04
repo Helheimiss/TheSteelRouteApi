992c2530657de86710365bd403d74616569bf06e
Helheimiss <171362259+Helheimiss@users.noreply.github.com>
2026-03-04 23:03:49 +0300

    changes

--- src/api.cpp
+++ src/api.cpp
@@ -9,7 +9,6 @@
 
 void api::user::login(const drogon::HttpRequestPtr &req, HttpResponseCallback &&callback, std::string &&login, std::string &&password) const {
     auto resp = drogon::HttpResponse::newHttpResponse(drogon::HttpStatusCode::k200OK, drogon::ContentType::CT_TEXT_PLAIN);
-    auto requestBody = req->getJsonObject();
 
     auto usr = User::findUser(login, password);
 
