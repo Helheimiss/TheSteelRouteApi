02d4f4e18f2f4a500d4880a07719da6b478ed27d
user <user@510EC12>
2026-03-05 10:16:14 +0300

    changes

--- include/api.hpp
+++ include/api.hpp
@@ -5,6 +5,31 @@
 using HttpResponseCallback = std::function<void(const drogon::HttpResponsePtr &)>;
 
 namespace api {
+class buses : public drogon::HttpController<buses> {
+
+};
+
+
+class drivers : public drogon::HttpController<drivers> {
+
+};
+
+
+class expenses : public drogon::HttpController<expenses> {
+
+};
+
+
+class orders : public drogon::HttpController<orders> {
+
+};
+
+
+class payments : public drogon::HttpController<payments> {
+
+};
+
+
 class user : public drogon::HttpController<user> {
 public:
     METHOD_LIST_BEGIN
